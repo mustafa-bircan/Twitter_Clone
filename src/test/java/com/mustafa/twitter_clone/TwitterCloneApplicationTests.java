42a9a19dfaced2c696489b440b7a3b68fad165e3
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-29 09:49:45 +0300

    Initial Commit

---
+++ src/test/java/com/mustafa/twitter_clone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.twitter_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
