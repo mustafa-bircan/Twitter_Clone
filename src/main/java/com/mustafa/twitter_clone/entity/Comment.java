07067e9212cefa1f975990e5ccfe62bbebc32e31
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-29 19:29:28 +0300

    feat: Created Comment entity with relationships to User and Tweet

---
+++ src/main/java/com/mustafa/twitter_clone/entity/Comment.java
@@ -0,0 +1,41 @@
+package com.mustafa.twitter_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "tweet_id", nullable = false)
+    private Tweet tweet;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+}
