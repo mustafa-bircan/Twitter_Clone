dec1f7de4a7a95c5a822743e8f2acae9a09a2c3a
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-29 19:30:18 +0300

    feat: Created Notification entity with relationship to User

---
+++ src/main/java/com/mustafa/twitter_clone/entity/Notification.java
@@ -0,0 +1,37 @@
+package com.mustafa.twitter_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User receiver;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+}
