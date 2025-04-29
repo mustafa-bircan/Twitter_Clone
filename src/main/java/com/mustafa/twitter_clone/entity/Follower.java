5498caa690b63538a22c5483add910636ecd9058
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-29 19:29:59 +0300

    feat: Created Follower entity with relationships to User (follower and following)

---
+++ src/main/java/com/mustafa/twitter_clone/entity/Follower.java
@@ -0,0 +1,38 @@
+package com.mustafa.twitter_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "followers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+}
