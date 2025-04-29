4a43fc0323e5bab3632de2cb9185fc464809b0aa
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-29 19:30:07 +0300

    feat: Created Media entity with a relationship to Tweet

---
+++ src/main/java/com/mustafa/twitter_clone/entity/Media.java
@@ -0,0 +1,33 @@
+package com.mustafa.twitter_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "media")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "tweet_id", nullable = false)
+    private Tweet tweet;
+
+    @Column(nullable = false)
+    private String mediaUrl;
+
+    @Column(nullable = false)
+    private String mediaType;
+
+}
