0cd575fb042f0055701f4e35800a9d9f0ad6ba4d
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-30 13:09:06 +0300

    Refactor: Move Media DTO conversion logic to DtoUtils

--- src/main/java/com/mustafa/twitter_clone/utils/DtoUtils.java
+++ src/main/java/com/mustafa/twitter_clone/utils/DtoUtils.java
@@ -1,6 +1,8 @@
 package com.mustafa.twitter_clone.utils;
 
+import com.mustafa.twitter_clone.dto.MediaResponseDto;
 import com.mustafa.twitter_clone.dto.UserResponseDto;
+import com.mustafa.twitter_clone.entity.Media;
 import com.mustafa.twitter_clone.entity.User;
 
 public class DtoUtils {
@@ -9,6 +11,17 @@ public class DtoUtils {
         dto.setId(user.getId());
         dto.setUsername(user.getUsername());
         dto.setEmail(user.getEmail());
+        return dto;
+    }
+
+    public static MediaResponseDto convertMediaToDto(Media media) {
+        MediaResponseDto dto = new MediaResponseDto();
+        dto.setId(media.getId());
+        dto.setUserId(media.getUser().getId());
+        dto.setUsername(media.getUser().getUsername());
+        dto.setTweetId(media.getTweet().getId());
+        dto.setMediaUrl(media.getMediaUrl());
+        dto.setMediaType(media.getMediaType());
         return dto;
     }
 }
